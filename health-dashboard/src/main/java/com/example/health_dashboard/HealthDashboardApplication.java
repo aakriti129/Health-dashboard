86726b6c930b4b8d40928dc39b07c060f5524429
Aakriti Sharma <aakritisharma510@gmail.com>
2025-02-27 02:19:41 +0530

    First initial commit

---
+++ health-dashboard/src/main/java/com/example/health_dashboard/HealthDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.health_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDashboardApplication.class, args);
+	}
+
+}
---
+++ health-dashboard/src/test/java/com/example/health_dashboard/HealthDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.health_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
